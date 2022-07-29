b33a02758e3f23f5f9b9e6e13877db7f7fc89f8b
Shaheer <shaheer.myhome@gmail.com>
2022-07-29 11:52:58 +0530

    added verifyusertoken function

--- middleware/verifyUserToken.js
+++ middleware/verifyUserToken.js
@@ -1,1 +1,38 @@
-/*SoonTm*/
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_PRIVATE_KEY
+const users = require('../database/schemas/users')
+
+const verifyUserToken = async(req, res, next) => {
+
+    const token = req.header('token')
+
+    if(!token) {
+        return res.status(401).json({success: false, message: "Token is expired or invalid.", code: 401})
+    }
+    try {
+
+    const tokenDetails = jwt.verify(token, JWT_SECRET)
+
+    if(tokenDetails.type === "user_account_token") {
+        const user = await users.findOne({_id: tokenDetails._id, pid: tokenDetails.pid}).select('-interests').select('-password').select('-pid')
+
+        if(!user) {
+            return res.status(401).json({success: false, message: "Token is expired or invalid.", code: 401})
+        }
+
+        req.authorized_account = user;
+        return next()
+
+    } else {
+
+        return res.status(401).json({success: false, message: "Token is expired or invalid.", code: 401})
+    }
+
+} catch {
+
+    return res.status(401).json({success: false, message: "Token is expired or invalid.", code: 401})
+
+}
+}
+
+module.exports = verifyUserToken;
