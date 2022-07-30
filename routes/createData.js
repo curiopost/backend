7958583b79a6ddbf2fd0bf081cc7ff40e52b966e
Shaheer <shaheer.myhome@gmail.com>
2022-07-30 16:12:02 +0530

    added create question endpoint

--- routes/createData.js
+++ routes/createData.js
@@ -4,7 +4,6 @@ const aerect = require('aerect.js');
 const verifyUserToken = require('../middleware/verifyUserToken');
 const posts = require('../database/schemas/posts');
 const replies = require('../database/schemas/replies');
-const abbreviate = require('number-abbreviate');
 const users = require('../database/schemas/users');
 
 router.post('/post', verifyUserToken, async (req, res) => {
@@ -131,6 +130,138 @@ await data.save()
 
     return res.status(500).json({ success: false, message: "Unexpected error occured on our end, please try again later.", code: 500 })
 }
+
+})
+
+router.post('/question', verifyUserToken, async (req, res) => {
+
+
+    const user =  req.authorized_account;
+    try {
+        const ID = aerect.generateID(11)
+    
+    let {title, content, attachment_url, topics, mentions } = req.body;
+    
+    
+    
+    var likes = []
+    
+    var type = "QUESTION"
+    
+    if(!title) {
+    
+    return res.status(400).json({success: false, message: "Title is required.", code: 400})
+    
+    }
+    
+    if(!content) {
+    
+        return res.status(400).json({success: false, message: "Content is required.", code: 400})
+    
+    }
+    
+    if(attachment_url && !attachment_url.toLowerCase().startsWith("https://res.cloudinary")) {
+    
+    return res.status(400).json({success: false, message: "Attachment URL must be from cloudinary.", code: 400})
+    
+    }
+    
+    if(mentions && !Array.isArray(mentions)) {
+    
+    return res.status(400).json({success: false, message: "Mentions must be an array.", code: 400})
+    
+    }
+    
+    if(topics && !Array.isArray(topics)) {
+    
+        return res.status(400).json({success: false, message: "Topics must be an array.", code: 400})
+    
+    
+    }
+    
+    
+    if(!mentions) {
+        mentions = []
+    }
+    
+    if(!topics) {
+        topics = []
+    }
+    
+    
+    user.followers.forEach(u => {
+    
+        users.findOne({_id: u}, async (err, data) => {
+    
+            if(!data) return;
+    
+            const notification_object = {
+              type: 'following_user_question_create',
+              created_at: Date.now(),
+              title: title,
+              url: `/questions/${ID}`,
+              reason: `${req.authorized_account.name} posted a new question.`,
+              type: type
+    
+            }
+    
+            data.notifications.push(notification_object)
+            await data.save()
+        })
+    })
+    
+        mentions.forEach(m => {
+    
+    users.findOne({username: m}, async (err, data) => {
+    
+    if(!data) return;
+    const notification_object = {
+        type: 'user_mention_you_in_question',
+        created_at: Date.now(),
+        title: title,
+        url: `/questions/${ID}`,
+        reason: `${req.authorized_account.name} mentioned you in a new question.`,
+        type: type
+    
+      }
+    
+      data.notifications.push(notification_object)
+    await data.save()
+    
+    
+    
+    })
+    
+        })
+        
+      
+    
+        const post_object = {
+            _id: ID,
+            user_id: req.authorized_account._id,
+            title: title,
+            content: content,
+            attachment_url: attachment_url || null,
+            created_at: Date.now(),
+            type: type,
+            mentions: mentions || [],
+            topics: topics || [],
+            likes: likes
+        }
+    
+        await posts.create(post_object)
+    
+        return res.status(201).json({success: true, message: "Question successfully created.", url: `/questions/${ID}`, code: 200})
+    
+    } catch(e) {
+        console.error(e)
+    
+        return res.status(500).json({ success: false, message: "Unexpected error occured on our end, please try again later.", code: 500 })
+    }
+
+
+
+
 
 })
 
