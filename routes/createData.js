80bd053ed04656edd6a04ef6d9a86d300fdb9b90
Shaheer <shaheer.myhome@gmail.com>
2022-08-01 11:29:47 +0530

    added create reply endpoint

--- routes/createData.js
+++ routes/createData.js
@@ -265,4 +265,110 @@ router.post('/question', verifyUserToken, async (req, res) => {
 
 })
 
+router.post('/reply', verifyUserToken, async(req, res) => {
+
+    const user =  req.authorized_account 
+
+    let {replied_to, content, attachment_url, topics, mentions} = req.body;
+
+    try {
+
+    
+
+    const user_id = user._id;
+    const created_at = Date.now()
+    const ID = aerect.generateID(11)
+
+    if(!replied_to) {
+
+        return res.status(400).json({success: false, message: "No question or post exists to reply.", code: 400})
+    }
+
+    if(!content) {
+        return res.status(400).json({success: false, message: "Content is required.", code: 400})
+    }
+
+    if(attachment_url && !attachment_url.toLowerCase().startsWith("https://res.cloudinary")) {
+    
+    return res.status(400).json({success: false, message: "Attachment URL must be from cloudinary.", code: 400})
+    
+    }
+
+    if(mentions && !Array.isArray(mentions)) {
+    
+        return res.status(400).json({success: false, message: "Mentions must be an array.", code: 400})
+        
+        }
+        
+        if(topics && !Array.isArray(topics)) {
+        
+            return res.status(400).json({success: false, message: "Topics must be an array.", code: 400})
+        
+        
+        }
+
+        const doesPostExist = await posts.findOne({_id: replied_to})
+
+        if(!doesPostExist) {
+            return res.status(400).json({success: false, message: "No question or post exists to reply.", code: 400})
+        }
+        if(!mentions) {
+            mentions = []
+        }
+        
+        if(!topics) {
+            topics = []
+        }
+
+
+
+       mentions.forEach(m => {
+
+users.findOne({username: m}, async (err, data) => {
+
+    if(!data) return;
+
+    const notification_object = {
+        type: 'user_mention_you_in_a_reply',
+        created_at: Date.now(),
+        title: `${req.authorized_account.name} mentioned you in a reply.`,
+        url: `/replies/${ID}`,
+        reason: `${req.authorized_account.name} mentioned you in a new reply.`,
+        type: 'REPLY'
+    
+
+    }
+
+    data.notifications.push(notification_object)
+    await data.save()
+})
+
+       })
+
+       const reply_object = {
+_id: ID,
+user_id: user_id,
+replied_to: replied_to,
+content: content,
+attachment_url: attachment_url || null,
+topics: topics || [],
+mentions: mentions || [],
+likes: [],
+created_at: created_at
+
+
+       }
+
+       await replies.create(reply_object)
+
+       return res.status(200).json({success: true, message: "Reply successfully added.", url: `/replies/${ID}`,code: 200})
+    } catch(e) {
+
+        console.error(e)
+    
+        return res.status(500).json({ success: false, message: "Unexpected error occured on our end, please try again later.", code: 500 })
+    }
+
+})
+
 module.exports = router;
