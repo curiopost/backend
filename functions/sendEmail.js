da307fba0e8e1981c3987c9506b0fcf918de3f9e
Shaheer <shaheer.myhome@gmail.com>
2022-08-03 11:54:26 +0530

    fixed sendEmail function

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -38,13 +38,7 @@ const sendemail = async(message, subject, reciever) => {
         subject: subject,
         text: message
       };
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-    })
+      return transporter.sendMail(mailOptions)
 }
 
 module.exports = sendemail;
