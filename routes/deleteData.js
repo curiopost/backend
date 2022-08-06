5d6bb35477bcfe1b7b6540ae3c1518ce63e0a006
Shaheer <shaheer.myhome@gmail.com>
2022-08-06 22:13:08 +0530

    formatted code for readability

--- routes/deleteData.js
+++ routes/deleteData.js
@@ -88,30 +88,30 @@ router.delete('/reply', verifyUserToken, async (req, res) => {
 router.delete('/account', antispamauth, verifyUserToken, async (req, res) => {
     try {
 
-        
+
         const user = req.authorized_account
 
-        const {password, feedback} = req.body;
-        if(!password) {
-            return res.status(400).json({success: false, message: "Password cannot be empty.", code: 400})
+        const { password, feedback } = req.body;
+        if (!password) {
+            return res.status(400).json({ success: false, message: "Password cannot be empty.", code: 400 })
         }
 
-        const getUser = await users.findOne({_id: user._id})
+        const getUser = await users.findOne({ _id: user._id })
 
         const isPasswordValid = await bcrypt.compare(password, getUser.password)
 
-        if(!isPasswordValid) {
-            return res.status(400).json({success: false, message: "Password is incorrect.", code: 400})
+        if (!isPasswordValid) {
+            return res.status(400).json({ success: false, message: "Password is incorrect.", code: 400 })
         }
 
-        if(feedback) {
+        if (feedback) {
 
             const f_id = aerect.generateID(11)
-            await feedbacks.create({_id: f_id, content: feedback, created_at: Date.now()})
+            await feedbacks.create({ _id: f_id, content: feedback, created_at: Date.now() })
         }
 
-        const getReplies = await replies.find({user_id: user._id})
-        const getPosts = await posts.find({user_id: user._id})
+        const getReplies = await replies.find({ user_id: user._id })
+        const getPosts = await posts.find({ user_id: user._id })
 
         getReplies.forEach(async gr => {
             await gr.delete()
@@ -125,9 +125,9 @@ router.delete('/account', antispamauth, verifyUserToken, async (req, res) => {
 
         await sendEmail(`Hi ${user.name} \n\nWe are extremely sad to see that you leave Curiopost. If you dropped some feedback before you delete your account, we would read it and try to fix the mistake immediately. If you did not add any feedback you can always drop us a tweet(@curiopost) and we will try to fix it as soon as possible. Keep in mind that you can always register again at: https://curiopost.live/register .\n\nThanks - The Curiopost Team.`, "Curiopost - Account has been deleted.", user.email)
 
-        return res.status(200).json({success: true, message: "Account has been deleted, might take a few more seconds for our servers to delete your data completely.", code: 200})
+        return res.status(200).json({ success: true, message: "Account has been deleted, might take a few more seconds for our servers to delete your data completely.", code: 200 })
 
-       
+
 
     } catch (e) {
         console.error(e)
